1ad22e373378163332c0d9d1e6db302d5b719e83
alex31bel <alex@31bel.ru>
2023-02-12 20:08:56 +0300

    output to a separate file

--- main.tf
+++ main.tf
@@ -47,10 +47,3 @@ resource "yandex_vpc_subnet" "subnet-1" {
   network_id     = yandex_vpc_network.network-1.id
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
-
-output "internal_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
-}
-output "external_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
-}
