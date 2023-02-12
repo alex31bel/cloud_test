7bd5fc0fc741431ebb0bf4c40d5823cfd7cb5ae0
alex31bel <alex@31bel.ru>
2023-02-12 20:11:27 +0300

    resource to a separate file

--- main.tf
+++ main.tf
@@ -12,38 +12,3 @@ provider "yandex" {
   folder_id = "xxx"
   zone      = "ru-central1-a"
 }
-
-resource "yandex_compute_instance" "vm-1" {
-  name = "terraform1"
-
-  resources {
-    cores  = 2
-    memory = 2
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = "fd87kbts7j40q5b9rpjr"
-    }
-  }
-
-  network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
-    nat       = true
-  }
-  
-  metadata = {
-    user-data = "${file("./meta.yaml")}"
-  }
-
-}
-resource "yandex_vpc_network" "network-1" {
-  name = "network1"
-}
-
-resource "yandex_vpc_subnet" "subnet-1" {
-  name           = "subnet1"
-  zone           = "ru-central1-b"
-  network_id     = yandex_vpc_network.network-1.id
-  v4_cidr_blocks = ["192.168.10.0/24"]
-}
