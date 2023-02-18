35a6326edeb6a83ec6ac6dfde649610c630445d4
alex31bel <alex@31bel.ru>
2023-02-18 18:56:10 +0300

    add vm-2

--- resource.tf
+++ resource.tf
@@ -36,3 +36,43 @@ resource "yandex_vpc_subnet" "subnet-1" {
   network_id     = yandex_vpc_network.network-1.id
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
+
+
+resource "yandex_compute_instance" "vm-2" {
+  name = "terraform2"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "${var.image_id_ya}"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-2.id
+    nat       = true
+  }
+  
+  metadata = {
+    user-data = "${file("./main.yaml")}"
+  }
+
+  scheduling_policy {
+    preemptible = true #Создание прерываемой ВМ для экономии баланса при обучении
+  }
+
+}
+resource "yandex_vpc_network" "network-2" {
+  name = "network2"
+}
+
+resource "yandex_vpc_subnet" "subnet-2" {
+  name           = "subnet2"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-2.id
+  v4_cidr_blocks = ["192.168.20.0/24"]
+}
